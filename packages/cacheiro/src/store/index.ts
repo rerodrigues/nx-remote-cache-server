3a599add286e31a2a21b8bcc0d852ef2f880c334
Renato Rodrigues <renato@renatorodrigues.com>
2026-06-06 11:03:24 +0200

    refactor(cacheiro): remove null-guards in createStore, validated by AJV

--- packages/cacheiro/src/store/index.ts
+++ packages/cacheiro/src/store/index.ts
@@ -10,18 +10,12 @@ export type { Store };
 export function createStore(): Store {
   switch (cfg.store.type) {
     case 'filesystem':
-      if (!cfg.store.filesystem)
-        throw new Error('store.filesystem config is required when store.type is "filesystem"');
       return new FileSystemStore(cfg.store.filesystem);
     case 's3':
-      if (!cfg.store.s3) throw new Error('store.s3 config is required when store.type is "s3"');
       return new S3Store(cfg.store.s3);
     case 'gcs':
-      if (!cfg.store.gcs) throw new Error('store.gcs config is required when store.type is "gcs"');
       return new GcsStore(cfg.store.gcs);
     case 'azure':
-      if (!cfg.store.azure)
-        throw new Error('store.azure config is required when store.type is "azure"');
       return new AzureStore(cfg.store.azure);
   }
 }
